7126371551e3f565dfcb4a7f18fcb9da61eaf982
aakriti-gulati <80571556+aakriti-gulati@users.noreply.github.com>
2022-03-02 14:24:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.json());
 
 app.get('/', (req,res)=>{
     res.status(200).send("Hello Programmers");
